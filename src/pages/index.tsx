25c03ead34e0a1548a4abb5bd7f66754d44cacc6
jakeorch <jakeorchanianatjake@gmail.com>
2025-06-04 17:35:55 -0700

    improve design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,60 +19,67 @@ export default function Home() {
     }, 100);
 
     return () => clearInterval(interval);
-  }, [timerOn, seconds]);
+  }, [timerOn, seconds, gpmInput, gpmCustom]);
 
   useEffect(() => {
     setCalculatedGallons(
       (seconds / 60) * (gpmInput !== "custom" ? Number(gpmInput) : gpmCustom)
     );
-  }, [seconds]);
+  }, [seconds, gpmInput, gpmCustom]);
 
-  // 2.5 GPM = high flow?, US federal regulation since 1992
-  // 2.0 GPM = medium flow?, EPA WaterSense program
-  // 1.8 GPM = low flow?, maximum in california & other states
+  // 2.5 GPM: US federallly regulated max since 1992
+  // 2.0 GPM: EPA WaterSense program
+  // 1.8 GPM: maximum in california & some other states
 
   return (
     <>
       <h1 className="text-3xl font-bold">Shower Timer</h1>
 
       <div className="mx-auto w-full md:w-96">
-        <div className="flex gap-2 mt-8">
-          <select
-            onChange={(e) => setGPMInput(e.currentTarget.value)}
-            value={gpmInput}
-            className="selectArrows bg-transparent w-full text-sm md:text-base text-center rounded-md border-2 border-gray-300 hover:bg-gray-200 focus:bg-gray-200 px-3 py-1.5 transition"
-          >
-            <optgroup label="Select shower head's gallons per minute">
-              <option value="1.5">
-                Very low flow (1.5 gal/min, 5.7 L/min)
-              </option>
-              <option value="1.8">Low flow (1.8 gal/min, 6.8 L/min)</option>
-              <option value="2.0">Medium flow (2.0 gal/min, 7.6 L/min)</option>
-              <option value="2.5">High flow (2.5 gal/min, 9.5 L/min)</option>
-              <option value="3.0">
-                Very high flow (3.0 gal/min, 11.4 L/min)
-              </option>
-              <option value="custom">Custom</option>
-            </optgroup>
-          </select>
+        <div className="w-full mt-8">
+          <span className="text-sm text-gray-600 text-left">
+            Select your shower head's flow rate
+          </span>
+          <div className="flex gap-2">
+            <select
+              onChange={(e) => setGPMInput(e.currentTarget.value)}
+              value={gpmInput}
+              className="selectArrows bg-transparent w-full text-sm md:text-base text-center rounded-md border-2 border-gray-300 hover:bg-gray-200 focus:bg-gray-200 px-3 py-1.5 transition"
+            >
+              <optgroup label="Select your shower head's flow rate">
+                <option value="1.5">
+                  Very low flow (1.5 gal/min, 5.7 L/min)
+                </option>
+                <option value="1.8">Low flow (1.8 gal/min, 6.8 L/min)</option>
+                <option value="2.0">
+                  Medium flow (2.0 gal/min, 7.6 L/min)
+                </option>
+                <option value="2.5">High flow (2.5 gal/min, 9.5 L/min)</option>
+                <option value="3.0">
+                  Very high flow (3.0 gal/min, 11.4 L/min)
+                </option>
+                <option value="custom">Custom</option>
+              </optgroup>
+            </select>
 
-          <input
-            className={`${
-              gpmInput == "custom" ? "" : "hidden"
-            } bg-transparent w-1/4 text-sm md:text-base rounded-md border-2 border-gray-300 hover:bg-gray-200 focus:bg-gray-200 transition px-3 py-2`}
-            onInput={(e) => setGPMCustom(Number(e.currentTarget.value))}
-            value={gpmCustom}
-            type="number"
-            placeholder="GPM"
-            min={0.1}
-            max={10}
-            step={0.1}
-          />
+            <input
+              className={`${
+                gpmInput == "custom" ? "" : "hidden"
+              } bg-transparent w-1/4 text-sm md:text-base rounded-md border-2 border-gray-300 hover:bg-gray-200 focus:bg-gray-200 transition px-3 py-2`}
+              onInput={(e) => setGPMCustom(Number(e.currentTarget.value))}
+              value={gpmCustom}
+              type="number"
+              placeholder="GPM"
+              min={0.1}
+              max={10}
+              step={0.1}
+            />
+          </div>
         </div>
 
         <div className="flex gap-2 mt-2">
           <button
-            className="w-full text-sm md:text-base border-2 border-gray-300 hover:bg-gray-200 active:bg-gray-300 cursor-pointer rounded-md px-3 py-1.5 transition"
+            className="w-full text-sm md:text-base text-white bg-blue-500 hover:bg-blue-600/90 active:bg-blue-700/80 cursor-pointer rounded-md px-3 py-1.5 transition"
             onClick={() => {
               setTimerOn(!timerOn);
             }}
@@ -80,13 +87,15 @@ export default function Home() {
             {timerOn ? "Pause" : seconds > 0 ? "Unpause" : "Start"}
           </button>
           <button
-            className="w-full text-sm md:text-base border-2 border-gray-300 hover:bg-gray-200 active:bg-gray-300 cursor-pointer rounded-md px-3 py-1.5 transition"
+            className={`${
+              seconds > 0 ? "" : "hidden"
+            } w-full text-sm md:text-base text-white bg-blue-500 hover:bg-blue-600/90 active:bg-blue-700/80 cursor-pointer rounded-md px-3 py-1.5 transition`}
             onClick={() => {
               setSeconds(0);
               setTimerOn(false);
             }}
           >
-            Restart
+            Reset
           </button>
         </div>
       </div>
@@ -100,7 +109,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="text-5xl md:text-6xl leading-[0.9] font-bold mt-8">
+      <div className="text-5xl md:text-6xl leading-[0.9] font-bold mt-10">
         {seconds.toFixed(1)}
       </div>
       <div className="text-lg">seconds</div>
